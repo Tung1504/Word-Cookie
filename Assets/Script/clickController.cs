db6105be0c65c0a86b384761a61fd42605852789
Tung <natsu1504@gmail.com>
2020-08-19 12:37:27 +0700

    add basic game

---
+++ Assets/Script/clickController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickController : MonoBehaviour
+{
+   
+
+    void OnMouseDown()
+    {
+        
+        gameMaster.currentWord += GetComponent<TextMesh>().text;
+        gameMaster.letterNum += 1;
+        gameMaster.selectLetter[gameMaster.letterNum] = GetComponent<TextMesh>().text;
+    }
+}
